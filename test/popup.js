cd3ae77eb8d2c4177ce8d36a8a155386b5ba7a32
Jackson Kendall <jackson.kendall@icloud.com>
2024-04-19 11:47:03 -0600

    first commit

---
+++ test/popup.js
@@ -0,0 +1,11 @@
+window.onload = function() {
+    showPopup();
+};
+
+function showPopup() {
+    document.getElementById("popup").style.display = "block";
+}
+
+function closePopup() {
+    document.getElementById("popup").style.display = "none";
+}
